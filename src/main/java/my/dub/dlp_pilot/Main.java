0da7ab32b2e28c1c2f0790130cdeb60da5a39537
Dmytro Dubynskyi <dmytro.dubynskyi@limestonedigital.co>
2020-03-28 23:00:03 +0200

    Create project structure
    Add basic dependencies, main classes
    
    Signed-off-by: Dmytro Dubynskyi <dmytro.dubynskyi@limestonedigital.co>

---
+++ src/main/java/my/dub/dlp_pilot/Main.java
@@ -0,0 +1,12 @@
+package my.dub.dlp_pilot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Init");
+    }
+}
---
+++ src/main/java/my/dub/dlp_pilot/configuration/AppConfig.java
@@ -0,0 +1,7 @@
+package my.dub.dlp_pilot.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
